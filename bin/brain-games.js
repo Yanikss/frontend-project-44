d99053af740652b2319b30b5d15722c656b4257f
iana <ianakuchumova@MacBook-Air-iana.local>
2022-11-15 00:42:37 +0300

    fix some code errors in bin directory

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
-import makeGreeting from "../src/cli.js";
-makeGreeting();
+import makeGreeting from '../src/cli.js';
 
+console.log('Welcome to the Brain Games!');
+makeGreeting();
